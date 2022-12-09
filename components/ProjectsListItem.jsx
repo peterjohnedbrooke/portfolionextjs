d68bbfc108abc9edbdb8d694b9dafb51609ddb9d
Peter Edbrooke <peter.john.edbrooke@gmail.com>
2022-12-09 01:14:34 +0000

    added new styles to projects and project list component

--- components/ProjectsListItem.jsx
+++ components/ProjectsListItem.jsx
@@ -14,7 +14,7 @@ export default function ProjectsListItem({item, index}) {
 
   return (
     <>
-        <li  className={!!hasMoved ? `${styles.listItemMove}` : `${styles.listItem}`} onMouseEnter={(e) => handleHover(index)} onMouseLeave={(e) => handleHover(index)} key={index}>
+        <li key={index}  className={!!hasMoved ? `${styles.listItemMove}` : `${styles.listItem}`} onMouseEnter={(e) => handleHover(index)} onMouseLeave={(e) => handleHover(index)} key={index}>
             <a  href='#'>
                 <h5  className={styles.textUpper}>{item.fields.title}</h5>
                 <h5 className={!!hasMoved ? `${styles.textGradientMove}` : `${styles.textGradient}`}>{item.fields.title}</h5>
