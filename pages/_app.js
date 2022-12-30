ab6bd2f3a62f4c376b14514b94c778bdbf0c3b4c
Peter Edbrooke <peter.john.edbrooke@gmail.com>
2022-12-30 00:01:19 +0000

    fixes to viewport isssue, changes to responsive styling

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,18 @@ import App from "next/app";
 import NavBar from '../components/NavBar';
 import { useEffect } from 'react';
 import "bootstrap-icons/font/bootstrap-icons.css";
+import Head from 'next/head';
 
 const URL = process.env.STRAPIBASEURL;
 
 function MyApp({ Component, pageProps, logos }) {
   return (
     <>
+    <Head>
+    {/* <meta name="viewport" content="viewport-fit=cover" /> */}
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    </Head>
       <NavBar logos={logos}/>
       <Component {...pageProps} />
     </>
